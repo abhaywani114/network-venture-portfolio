9780a1c8ecca418f717141775bb684b97a5987c3
Abrar Ajaz Wani <abhaywani114@gmail.com>
2020-12-15 12:03:03 +0530

    Mail added

--- mail.php
+++ mail.php
@@ -1,19 +1,31 @@
 <?php
 require __DIR__.'/mail/class.phpmailer.php';
+$from = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$msg = nl2br($_POST['message']);
+
 $phpMailer = new PHPMailer(true);
+$phpMailer->SMTPDebug = 4; // Enable verbose debug output
 $phpMailer->isSMTP();
-$phpMailer->Host = "smtp.zoho.com";
+$phpMailer->Host = "smtp.zoho.in";
 $phpMailer->SMTPAuth = true;
 $phpMailer->Username = "support@network-venture.com";
-$phpMailer->Password = "Pluto0912HONEY";
+$phpMailer->Password = "jq8xbK8KHQei";
 $phpMailer->SMTPSecure = "tls";
 $phpMailer->Port = 587;
 $phpMailer->isHTML(true);
 $phpMailer->CharSet = "UTF-8";
-$phpMailer->setFrom("mail-user", "mail-name");
-$phpMailer->addAddress('admin@network-venture.com');//"mail-to");
-$phpMailer->Subject = "subject";
-$phpMailer->Body = "mail-body";
+$phpMailer->setFrom("support@network-venture.com", "Support");
+$phpMailer->addAddress('support@network-venture.com');//"mail-to");
+$phpMailer->Subject = "Contact Request | $subject";
+$phpMailer->Body = <<<EOD
+Sender: $from <br/>
+From Email: $email <br/>
+Msg: <br/>
+$msg
+EOD;
+
 $phpMailer->send();
 echo json_encode(['msg' => 'OK']);
 ?>
